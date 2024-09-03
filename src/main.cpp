67c4957a6b5f2b1bc21ec5170192e40551647ec1
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-09-03 11:09:56 -0400

    removing some old code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
-   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+   6E400001-B5A3-F393-E0A9-E50E24DCCA9E Has a characteristic of:
+   6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send
+   data with  "NOTIFY"
 
    The design of creating the BLE server is:
    1. Create a BLE Server
@@ -16,82 +12,71 @@
    5. Start the service.
    6. Start advertising.
 
-   In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second. 
+   In this example rxValue is the data received (only accessible inside that
+   function). And txValue is the data to be sent, in this example just a byte
+   incremented every second.
 */
-
-/** NimBLE differences highlighted in comment blocks **/
-
-/*******original********
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-***********************/
-#include <NimBLEDevice.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <NimBLEDevice.h>
 
 BLEServer *pServer = NULL;
-BLECharacteristic * pTxCharacteristic;
+BLECharacteristic *pTxCharacteristic;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 uint8_t txValue = 0;
 uint8_t sensor_value = 0;
 
-// ESP32 ADC1 channel 0 is GPIO 36
+// ESP32 ADC1 channel 0 is GPIO 36 for Potentiometer
 const int adc_pin = 36;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+/**  None of these are required as they will be handled by the library with
+ *defaults. **
+ **                       Remove as you see fit for your needs */
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) { deviceConnected = true; };
 
-/**  None of these are required as they will be handled by the library with defaults. **
- **                       Remove as you see fit for your needs                        */  
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
+  void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
   /***************** New - Security handled here ********************
   ****** Note: these are the same return values as defaults ********/
-    uint32_t onPassKeyRequest(){
-      Serial.println("Server PassKeyRequest");
-      return 123456; 
-    }
+  uint32_t onPassKeyRequest() {
+    Serial.println("Server PassKeyRequest");
+    return 123456;
+  }
 
-    bool onConfirmPIN(uint32_t pass_key){
-      Serial.print("The passkey YES/NO number: ");Serial.println(pass_key);
-      return true; 
-    }
+  bool onConfirmPIN(uint32_t pass_key) {
+    Serial.print("The passkey YES/NO number: ");
+    Serial.println(pass_key);
+    return true;
+  }
 
-    void onAuthenticationComplete(ble_gap_conn_desc desc){
-      Serial.println("Starting BLE work!");
-    }
+  void onAuthenticationComplete(ble_gap_conn_desc desc) {
+    Serial.println("Starting BLE work!");
+  }
   /*******************************************************************/
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string rxValue = pCharacteristic->getValue();
 
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++)
-          Serial.print(rxValue[i]);
+    if (rxValue.length() > 0) {
+      Serial.println("*********");
+      Serial.print("Received Value: ");
+      for (int i = 0; i < rxValue.length(); i++)
+        Serial.print(rxValue[i]);
 
-        Serial.println();
-        Serial.println("*********");
-      }
+      Serial.println();
+      Serial.println("*********");
     }
+  }
 };
 
 uint16_t readADC() {
@@ -99,12 +84,11 @@ uint16_t readADC() {
   return adc_value;
 }
 
-
 void setup() {
   Serial.begin(115200);
   // while(!Serial.available());
 
-  analogReadResolution(12); // 12 bits of resolution for ADC readings
+  analogReadResolution(12);       // 12 bits of resolution for ADC readings
   analogSetAttenuation(ADC_11db); // 0-3.3V range for ADC readings
 
   // Create the BLE Device
@@ -119,33 +103,30 @@ void setup() {
 
   // Create a BLE Characteristic
   pTxCharacteristic = pService->createCharacteristic(
-                                        CHARACTERISTIC_UUID_TX,
-                                    /******* Enum Type NIMBLE_PROPERTY now *******      
-                                        BLECharacteristic::PROPERTY_NOTIFY
-                                        );
-                                    **********************************************/  
-                                        NIMBLE_PROPERTY::NOTIFY
-                                       );
-                                    
-  /***************************************************   
-   NOTE: DO NOT create a 2902 descriptor 
-   it will be created automatically if notifications 
-   or indications are enabled on a characteristic.
-   
-   pCharacteristic->addDescriptor(new BLE2902());
-  ****************************************************/                  
+      CHARACTERISTIC_UUID_TX,
+      /******* Enum Type NIMBLE_PROPERTY now *******
+          BLECharacteristic::PROPERTY_NOTIFY
+          );
+      **********************************************/
+      NIMBLE_PROPERTY::NOTIFY);
 
-  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-                                            CHARACTERISTIC_UUID_RX,
-                                    /******* Enum Type NIMBLE_PROPERTY now *******       
-                                            BLECharacteristic::PROPERTY_WRITE
-                                            );
-                                    *********************************************/  
-                                            NIMBLE_PROPERTY::WRITE
-                                            );
+  /***************************************************
+   NOTE: DO NOT create a 2902 descriptor
+   it will be created automatically if notifications
+   or indications are enabled on a characteristic.
+
+   pCharacteristic->addDescriptor(new BLE2902());
+  ****************************************************/
+
+  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID_RX,
+      /******* Enum Type NIMBLE_PROPERTY now *******
+              BLECharacteristic::PROPERTY_WRITE
+              );
+      *********************************************/
+      NIMBLE_PROPERTY::WRITE);
 
   pRxCharacteristic->setCallbacks(new MyCallbacks());
-
 
   // Start the service
   pService->start();
@@ -157,29 +138,29 @@ void setup() {
 
 void loop() {
 
+  if (deviceConnected) {
 
-    if (deviceConnected) {
+    uint16_t sensor_value = readADC();
+    uint8_t buffer[2];
+    buffer[0] = sensor_value & 0xFF;
+    buffer[1] = (sensor_value >> 8) & 0xFF;
+    // convert to uint8_t buffer with const pointer
+    pTxCharacteristic->setValue(buffer, 2);
+    pTxCharacteristic->notify();
+    delay(10); // bluetooth stack will go into congestion, if too many packets
+               // are sent
+  }
 
-        uint16_t sensor_value = readADC();
-        uint8_t buffer[2];
-        buffer[0] = sensor_value & 0xFF;
-        buffer[1] = (sensor_value >> 8) & 0xFF;
-        // convert to uint8_t buffer with const pointer
-        pTxCharacteristic->setValue(buffer, 2);
-        pTxCharacteristic->notify();
-        delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-    }
-
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
 }
